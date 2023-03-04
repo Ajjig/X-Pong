3bb539a30124602fb8db358ef68297c4277cc462
Ilyasse Idkhebbach <iidkhebb@e2r9p9.1337.ma>
2023-03-04 02:17:07 +0100

    added JWT ENV SECRET

--- src/nestjs/tsc/src/auth/auth.module.ts
+++ src/nestjs/tsc/src/auth/auth.module.ts
@@ -6,10 +6,13 @@ import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from './prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.startegy';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports:[ PassportModule.register({ defaultStrategy: '42' }), JwtModule.register ({
-  secret  : 'secret', // TODO: move to env
+  secret  : process.env.JWT_SECRET, // TODO: move to env
     signOptions : { expiresIn : '60s' },
   })],
   providers: [FortyTwoStrategy, AuthService, PrismaService, JwtStrategy],
--- src/nestjs/tsc/src/auth/jwt.startegy.ts
+++ src/nestjs/tsc/src/auth/jwt.startegy.ts
@@ -1,6 +1,9 @@
 import { PassportStrategy } from '@nestjs/passport';
 import {ExtractJwt, Strategy} from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy)
@@ -9,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy)
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: 'secret', // TODO: move to env
+            secretOrKey: process.env.JWT_SECRET, // TODO: move to env
         });
     }
 
