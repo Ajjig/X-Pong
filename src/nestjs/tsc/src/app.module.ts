fe757ac05aace2ea36ef61f81cf77a30434749eb
Ajjig <majjig@pm.me>
2023-03-06 23:11:02 +0100

    add module

--- src/nestjs/tsc/src/app.module.ts
+++ src/nestjs/tsc/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { GameModule } from './sync/game.module';
 
 @Module({
-  imports: [ChatModule, AuthModule, UserModule],
+  imports: [ChatModule, AuthModule, UserModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
