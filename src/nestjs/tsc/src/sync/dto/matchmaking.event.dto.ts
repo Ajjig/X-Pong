57805074182444139b24080daca16dac44409c40
Ajjig <majjig@pm.me>
2023-03-06 21:15:52 +0100

    added matchmaking dto

---
+++ src/nestjs/tsc/src/sync/dto/matchmaking.event.dto.ts
@@ -0,0 +1,9 @@
+export class matchMakingEventDto {
+
+    OpponentUsername : string;
+    OpponentLevel : number;
+    OpponentRanking : number;
+    OpponentAvatarUrl : string;
+
+    // ... more fields here
+}
