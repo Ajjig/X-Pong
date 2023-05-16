23c01d3624f24068e8b397ea18c907734b79b3a8
Ajjig <majjig@pm.me>
2023-05-16 18:10:31 +0100

    :heavy_plus_sign: enableCors

--- src/nestjs/tsc/src/main.ts
+++ src/nestjs/tsc/src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import cookieParser from 'cookie-parser';
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
   await app.listen(3000);
 }
 bootstrap();
