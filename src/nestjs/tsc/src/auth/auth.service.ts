3650600979ca2c64a90a0a4ee34688c0dce5afd2
Ajjig <majjig@pm.me>
2023-05-19 16:36:00 +0100

    add updatetoken method

--- src/nestjs/tsc/src/auth/auth.service.ts
+++ src/nestjs/tsc/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException } from '@nestjs/common';
+import { Injectable, HttpException, BadRequestException } from '@nestjs/common';
 import { User, Prisma, PrismaClient } from '.prisma/client';
 import { PrismaService } from '../prisma.service';
 import { JwtService } from '@nestjs/jwt';
@@ -45,9 +45,40 @@ export class AuthService {
     });
   }
 
+  async findUserById(id: string): Promise<any> {
+    return this.prisma.user.findUnique({
+      where: { id: parseInt(id) },
+      select: {
+        email: true,
+        name: true,
+        username: true,
+        avatarUrl: true,
+        onlineStatus: true,
+        confirmed: true,
+        istwoFactor: true,
+      },
+    });
+  }
+
+  async findUserByEmail(email: string): Promise<any> {
+    return this.prisma.user.findUnique({
+      where: { email: email },
+      select: {
+        email: true,
+        name: true,
+        username: true,
+        avatarUrl: true,
+        onlineStatus: true,
+        confirmed: true,
+        istwoFactor: true,
+      },
+    });
+  }
+
+
   async findOrCreateUser(profile: any): Promise<any> {
-    let user = await this.findUserByUsername(
-      profile.username,
+    let user = await this.findUserByEmail(
+      profile.emails[0].value,
     );
 
     if (!user) {
@@ -68,10 +99,26 @@ export class AuthService {
   // }
 
   async login(user: any, res: Response) {
+
+    try {
+      const payload = { name: user.name, username: user.username, sub: user.id };
+      const userData = await this.findUserByUsername(user.username);
+      const token = this.JwtService.sign(payload);
+      res.cookie('jwt', token, { httpOnly: true });
+      res.redirect(process.env.FRONTEND_REDIRECT_LOGIN_URL);
+    } catch (error) {
+      throw new BadRequestException('ERROR:', error.message);
+    }
+  }
+
+  async logout(res: Response) {
+    res.clearCookie('jwt');
+    res.redirect(process.env.FRONTEND_REDIRECT_LOGOUT_URL);
+  }
+
+  async updateProfileAndToken(user: any, res: Response) : Promise<void> {
     const payload = { name: user.name, username: user.username, sub: user.id };
-    const userData = await this.findUserByUsername(user.username);
     const token = this.JwtService.sign(payload);
     res.cookie('jwt', token, { httpOnly: true });
-    res.redirect(process.env.FRONTEND_REDIRECT_LOGIN_URL);
   }
 }
