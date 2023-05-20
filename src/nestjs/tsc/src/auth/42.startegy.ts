14842edb5e53a8010dd8bb6e2ae4a4ed86f1f0c9
Mhamed Ajjig <majjig@pm.me>
2023-05-20 18:56:06 +0000

    Protect from invalid user data

--- src/nestjs/tsc/src/auth/42.startegy.ts
+++ src/nestjs/tsc/src/auth/42.startegy.ts
@@ -1,6 +1,6 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-42";
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import * as dotenv from 'dotenv';
 
@@ -9,16 +9,21 @@ dotenv.config();
 
 @Injectable()
 export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
+    logger : Logger;
     constructor(private authService: AuthService) {
         super({
             clientID: process.env.clientID,
             clientSecret: process.env.clientSecret,
             callbackURL: process.env.callbackURL,
         });
+        this.logger = new Logger('FORTY-TWO-STRATEGY');
     }
 
-    async validate(accessToken: string, refreshToken: string, profile: any, done: Function,) {
+    async validate(accessToken: string, refreshToken: string, profile: any, done: Function) {
         const user = await this.authService.findOrCreateUser(profile);
+        if (user == undefined || user == null) {
+            throw new BadRequestException();
+        }
         done(null, user);
     }
 }
