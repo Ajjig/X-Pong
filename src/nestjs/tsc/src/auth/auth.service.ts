1c12124dfee206d8691812471d99fc7f926c5e85
Mhamed Ajjig <majjig@pm.me>
2023-05-21 14:18:06 +0000

    change cookie path

--- src/nestjs/tsc/src/auth/auth.service.ts
+++ src/nestjs/tsc/src/auth/auth.service.ts
@@ -126,7 +126,7 @@ export class AuthService {
     const payload = { username: user.username, uid: user.id};
     this.logger.log(`Updated JWT for ${user.username}`);
     const token = this.JwtService.sign(payload);
-    res.cookie('jwt', token, { httpOnly: true });
+    res.cookie('jwt', token, { httpOnly: true, path: '/auth/42' });
     res.status(200).send({ message: 'Username updated' });
   }
 }
