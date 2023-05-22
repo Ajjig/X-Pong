73a2a58a72059dae87f2715626cfcd6f6aca8d35
Mhamed Ajjig <majjig@pm.me>
2023-05-22 15:23:25 +0000

    feat: add catch-all filer

--- src/nestjs/src/exceptions/all.exception.catch.ts
+++ src/nestjs/src/exceptions/all.exception.catch.ts
@@ -1,9 +1,4 @@
-import {
-  ArgumentsHost,
-  Catch,
-  ExceptionFilter,
-  Logger,
-} from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { Response, Request } from 'express';
 
 @Catch()
@@ -17,21 +12,12 @@ export class AllExceptionFilter implements ExceptionFilter {
     const status = exception.status || 500;
 
     this.logger.warn(` ==> ${request.method} ${request.url}`);
-    try {
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-        message: exception.message,
-        extraInfo: exception.meta,
-      });
-    } catch (e) {
-      this.logger.error(e.message);
-      response.status(500).json({
-        statusCode: 500,
-        timestamp: new Date().toISOString(),
-        message: 'Internal Server Error',
-      });
-    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString() || new Date(),
+      path: request.url || request.originalUrl,
+      message: exception.message || 'Internal Server Error',
+    });
   }
 }
