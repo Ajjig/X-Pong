0710239835c830e87c2f793881cf4339003c5a83
Mhamed Ajjig <majjig@pm.me>
2023-05-22 00:35:26 +0000

    update generate id util

--- src/nestjs/src/sync/utils/generate.id.ts
+++ src/nestjs/src/sync/utils/generate.id.ts
@@ -1,18 +1,10 @@
-import { Game } from "../gateway/game";
+import { Game } from '../gateway/game';
+import * as rs from 'randomstring';
 
-
-export function makeId(games : Map<string, Game>) : string {
-    let result = '';
-    let length = 10;
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charsLength = chars.length;
-    let counter = 0;
-    while (counter < length) {
-      result += chars.charAt(Math.floor(Math.random() * charsLength));
-      counter++;
-    }
-    if (games.has(result)) {
-      return makeId(games);
-    }
-    return result;
+export function makeId(games: Map<string, Game>, length = 10): string {
+  let result = rs.generate(length);
+  if (games.has(result)) {
+    return makeId(games);
+  }
+  return result;
 }
