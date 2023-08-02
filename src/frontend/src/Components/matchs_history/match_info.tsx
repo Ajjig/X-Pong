29fe053329ec6af190dc5462cf04decd78903f5f
Rachid oudouch <rashidoudouch@gmail.com>
2023-08-02 15:35:05 +0100

    chnage colors

--- src/frontend/src/Components/matchs_history/match_info.tsx
+++ src/frontend/src/Components/matchs_history/match_info.tsx
@@ -3,26 +3,26 @@ import { Flex, Avatar, Title, Paper, Space } from "@mantine/core";
 
 export function Match_info({ player1, player2, score, result, children }: any) {
     return (
-        <Paper radius={20} bg={"cos_black.3"} p="sm">
+        <Paper radius={30} bg={"cos_black.1"} p="sm">
             <Flex p={5} align="center" justify="space-between">
                 <Flex align="center">
-                    <Avatar size={50} radius="xl" src="https://picsum.photos/201" />
+                    <Avatar size={40} radius="xl" src="https://picsum.photos/201" />
                     <Space w={10} />
-                    <Title color="gray.4" fz="md">
+                    <Title color="gray.4" fz="sm">
                         Username
                     </Title>
                 </Flex>
                 <Flex align="center">
                     <Title color="gray.4" fz="lg">
-                        {score?.player1} - {score?.player2}
+                        {score?.player1} -  {score?.player2}
                     </Title>
                 </Flex>
                 <Flex align="center">
-                    <Title color="gray.4" fz="md">
+                    <Title color="gray.4" fz="sm">
                         Username
                     </Title>
                     <Space w={10} />
-                    <Avatar size={50} radius="xl" src="https://picsum.photos/200" />
+                    <Avatar size={40} radius="xl" src="https://picsum.photos/200" />
                 </Flex>
             </Flex>
             {children && (
